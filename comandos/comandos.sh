b5a3126a732a9bfe070053de55b07cefb86424a3
Robson Vaamonde <robsonvaamonde@gmail.com>
2025-11-17 21:50:46 -0300

    Atualização

---
+++ comandos/comandos.sh
@@ -0,0 +1,25 @@
+1  poweroff
+2  curl -fsS https://dl.brave.com/install.sh | sh
+3  sudo apt install torbrowser-launcher 
+4  sudo apt update
+5  sudo apt install git vim notepadqq 
+6  sudo apt update
+7  sudo
+8  sudo -i
+9  sudo apt update 
+10  sudo apt upgrade 
+11  sudo apt dist-upgrade 
+12  sudo apt full-upgrade 
+13  sudo apt autoremove 
+14  sudo apt autoclean 
+15  sudo apt clean
+16  sudo apt update
+17  sudo apt install git vim pip python3
+18  git clone https://github.com/vaamonde/linuxmint
+19  git config --global user.name "Seu Usuário"
+20  git config --global user.email seuemail@seudominio.com
+21  git config --global core.editor vim
+22  git config --global merge.tool vimdiff
+23  git config --global color.ui true
+24  git config --list
+25  history
